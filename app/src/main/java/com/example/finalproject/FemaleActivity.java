22317b835cc100c99d11686ff86b7948879741e9
TheVeryGit <novikovyr1511@gmail.com>
2023-05-28 11:02:23 +0300

    Initial commit

---
+++ app/src/main/java/com/example/finalproject/FemaleActivity.java
@@ -0,0 +1,65 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class FemaleActivity extends AppCompatActivity {
+
+    private Button back;
+    private Button resultButton;
+    private TextView result;
+
+    public EditText heightIn;
+    private EditText weightIn;
+    private EditText ageIn;
+
+    private int height;
+    private int weight;
+    private int age;
+    private int calory;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_female);
+
+
+        back = findViewById(R.id.back);
+        resultButton = findViewById(R.id.resultButton);
+        weightIn = findViewById(R.id.weightIn);
+        ageIn = findViewById(R.id.ageIn);
+        result = findViewById(R.id.result);
+        heightIn = findViewById(R.id.heightIn);
+
+        back.setOnClickListener(listener);
+        resultButton.setOnClickListener(listener1);
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            Intent intent = new Intent(getApplicationContext(),SecondActivity.class);
+            startActivity(intent);
+        }
+    };
+
+    private View.OnClickListener listener1 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            height = Integer.parseInt(heightIn.getText().toString());
+            weight = Integer.parseInt(weightIn.getText().toString());
+            age = Integer.parseInt(ageIn.getText().toString());
+            calory = (height * 10 + weight * 6 - age * 5 - 161);
+
+            result.setText("Ваша суточная норма потребления в ккалориях:" + "\n" + String.valueOf(calory));
+        }
+    };
+}
---
+++ app/src/main/java/com/example/finalproject/SecondActivity.java
@@ -0,0 +1,45 @@
+package com.example.finalproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+public class SecondActivity extends AppCompatActivity {
+
+    private ImageButton male;
+    private ImageButton female;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+
+        male = findViewById(R.id.male);
+        female = findViewById(R.id.female);
+
+        male.setOnClickListener(listener);
+        female.setOnClickListener(listener1);
+    }
+
+    private View.OnClickListener listener = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            Intent intent = new Intent(getApplicationContext(), MaleActivity.class);
+            startActivity(intent);
+        }
+    };
+    private View.OnClickListener listener1 = new View.OnClickListener() {
+        @Override
+        public void onClick(View view) {
+
+            Intent intent1 = new Intent(getApplicationContext(),FemaleActivity.class);
+            startActivity(intent1);
+
+        }
+    };
+
+}
